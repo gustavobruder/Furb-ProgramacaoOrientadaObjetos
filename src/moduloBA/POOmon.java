d1c039fb74706dc0100091cb5cc1ab80f7f26400
gustavobruder <gugabruder@gmail.com>
2021-11-15 11:16:44 -0300

    Adicionado ataques agressivo e cruel com testes unitários

--- src/moduloBA/POOmon.java
+++ src/moduloBA/POOmon.java
@@ -1,5 +1,7 @@
 package moduloBA;
 
+import java.util.Random;
+
 public class POOmon {
     private String nome;
     private int energiaVital;
@@ -12,6 +14,29 @@ public class POOmon {
     public void ataqueBasico(POOmon pooMonDefensor) {
         int danoAtaque = 30;
         pooMonDefensor.setEnergiaVital(pooMonDefensor.getEnergiaVital() - danoAtaque);
+    }
+
+    public void ataqueAgressivo(POOmon pooMonDefensor) {
+        int danoAtaque = new Random().nextInt((99 - 40) + 1) + 40;
+
+        if (danoAtaque >= this.getEnergiaVital()) {
+            throw new IllegalArgumentException("Dano de ataque não pode ser maior ou igual a energia vital");
+        }
+
+        pooMonDefensor.setEnergiaVital(pooMonDefensor.getEnergiaVital() - danoAtaque);
+        this.setEnergiaVital(this.getEnergiaVital() - danoAtaque);
+    }
+
+    public void ataqueCruel(POOmon pooMonDefensor) {
+        int danoAtaque = new Random().nextInt((200 - 100) + 1) + 100;
+        int danoAtaqueBonificado = (int) (danoAtaque * 1.5);
+
+        if ((danoAtaque * 2) >= this.getEnergiaVital()) {
+            throw new IllegalArgumentException("Dano de ataque não pode ser maior ou igual ao dobro da energia vital");
+        }
+
+        pooMonDefensor.setEnergiaVital(pooMonDefensor.getEnergiaVital() - danoAtaqueBonificado);
+        this.setEnergiaVital(this.getEnergiaVital() - danoAtaque);
     }
 
     public int getEnergiaVital() {
--- src/moduloBA/Testes.java
+++ src/moduloBA/Testes.java
@@ -1,8 +1,9 @@
 package moduloBA;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Testes {
 
@@ -16,5 +17,64 @@ public class Testes {
 
         assertEquals(terra.getEnergiaVital(), 470);
         assertEquals(agua.getEnergiaVital(), 500);
+    }
+
+    @Test
+    public void deveCausarDanoComAtaqueAgressivo() {
+        POOmonH agua = new POOmonH("POOmon agua");
+        POOmonT terra = new POOmonT("POOmon terra");
+
+        // 40 a 99 dano
+        agua.ataqueAgressivo(terra);
+
+        assertTrue(terra.getEnergiaVital() > 400);
+        assertTrue(terra.getEnergiaVital() < 461);
+        assertTrue(agua.getEnergiaVital() > 400);
+        assertTrue(agua.getEnergiaVital() < 461);
+        assertEquals(agua.getEnergiaVital(), terra.getEnergiaVital());
+    }
+
+    @Test
+    public void deveCausarDanoComAtaqueCruel() {
+        POOmonH agua = new POOmonH("POOmon agua");
+        POOmonT terra = new POOmonT("POOmon terra");
+
+        // 100 a 200 dano
+        agua.ataqueCruel(terra);
+
+        assertTrue(terra.getEnergiaVital() > 199);
+        assertTrue(terra.getEnergiaVital() < 351);
+        assertTrue(agua.getEnergiaVital() > 299);
+        assertTrue(agua.getEnergiaVital() < 401);
+    }
+
+    @Test
+    public void naoDeveCausarDanoComAtaqueAgressivoCasoConsumoDoAtaqueForMaiorOuIgualAEnergiaVital() {
+        POOmonH agua = new POOmonH("POOmon agua");
+        POOmonT terra = new POOmonT("POOmon terra");
+
+        // 40 a 99 dano
+        agua.setEnergiaVital(40);
+        Executable ataque = () -> agua.ataqueAgressivo(terra);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, ataque);
+        assertEquals(exception.getMessage(), "Dano de ataque não pode ser maior ou igual a energia vital");
+        assertEquals(terra.getEnergiaVital(), 500);
+        assertEquals(agua.getEnergiaVital(), 40);
+    }
+
+    @Test
+    public void naoDeveCausarDanoComAtaqueCruelCasoConsumoDoAtaqueForMaiorOuIgualAoDobroDeEnergiaVital() {
+        POOmonH agua = new POOmonH("POOmon agua");
+        POOmonT terra = new POOmonT("POOmon terra");
+
+        // 100 a 200 dano
+        agua.setEnergiaVital(200); // dobro do consumo min (100)
+        Executable ataque = () -> agua.ataqueCruel(terra);
+
+        Exception exception = assertThrows(IllegalArgumentException.class, ataque);
+        assertEquals(exception.getMessage(), "Dano de ataque não pode ser maior ou igual ao dobro da energia vital");
+        assertEquals(terra.getEnergiaVital(), 500);
+        assertEquals(agua.getEnergiaVital(), 200);
     }
 }

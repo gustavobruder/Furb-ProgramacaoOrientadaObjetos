fffadd77c5c3f0d825561de4b50891d082a8c6b0
gustavobruder <gugabruder@gmail.com>
2021-11-30 11:32:15 -0300

    Adicionado testes unitários de poomons

--- src/moduloBA/TestesPOOmons.java
+++ src/moduloBA/TestesPOOmons.java
@@ -3,67 +3,36 @@ package moduloBA;
 import moduloBGame.Ambiente;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TestesPOOmons {
-
-    /*
-    Os testes foram escritos conforme o desenvolvimento das funcionalidades
-    Rodar os testes da forma que estão não trará um resultado bem-sucedido
-    */
 
     @Test
     public void deveCausarDanoComAtaqueBasico() {
         POOmonH agua = new POOmonH();
         POOmonT terra = new POOmonT();
+        agua.carregar(-480);
 
         // 30 dano
         agua.atacar(terra, Ambiente.AR);
 
         assertEquals(terra.getEnergia(), 470);
-        assertEquals(agua.getEnergia(), 500);
-    }
-
-    @Test
-    public void deveCausarDanoComAtaqueBasicoComBonusCasoForEmAmbienteDeOrigem() {
-        Ambiente ambiente = Ambiente.AGUA;
-        POOmonH agua = new POOmonH();
-        POOmonT terra = new POOmonT();
-
-        // 30 fixo + 20% pelo ambiente = 36 dano
-        agua.atacar(terra, ambiente);
-
-        assertEquals(terra.getEnergia(), 464);
-        assertEquals(agua.getEnergia(), 500);
-    }
-
-    @Test
-    public void deveCausarDanoReduzidoComAtaqueBasicoCasoDefensorForEmAmbienteDeOrigem() {
-        Ambiente ambiente = Ambiente.TERRA;
-        POOmonH agua = new POOmonH();
-        POOmonT terra = new POOmonT();
-
-        // 30 fixo - 10% pelo ambiente = 27 dano
-        agua.atacar(terra, ambiente);
-
-        assertEquals(terra.getEnergia(), 473);
-        assertEquals(agua.getEnergia(), 500);
+        assertEquals(agua.getEnergia(), 20);
     }
 
     @Test
     public void deveCausarDanoComAtaqueAgressivo() {
         POOmonH agua = new POOmonH();
         POOmonT terra = new POOmonT();
+        agua.carregar(-400);
 
         // 40 a 99 dano
         agua.atacar(terra, Ambiente.AR);
 
         assertTrue(terra.getEnergia() > 400);
         assertTrue(terra.getEnergia() < 461);
-        assertTrue(agua.getEnergia() > 400);
-        assertTrue(agua.getEnergia() < 461);
-        assertEquals(agua.getEnergia(), terra.getEnergia());
     }
 
     @Test
@@ -78,5 +47,25 @@ public class TestesPOOmons {
         assertTrue(terra.getEnergia() < 351);
         assertTrue(agua.getEnergia() > 299);
         assertTrue(agua.getEnergia() < 401);
+    }
+
+    @Test
+    public void deveCarregarImagensDeAcordoComEstadoVital() {
+        POOmonH agua = new POOmonH();
+
+        Image imgSaudavel = agua.getImagem();
+        assertNotNull(imgSaudavel);
+
+        agua.carregar(-150);
+        Image imgCansado = agua.getImagem();
+        assertNotNull(imgCansado);
+
+        agua.carregar(-200);
+        Image imgExausto = agua.getImagem();
+        assertNotNull(imgExausto);
+
+        agua.carregar(-150);
+        Image imgMorto = agua.getImagem();
+        assertNotNull(imgMorto);
     }
 } 
